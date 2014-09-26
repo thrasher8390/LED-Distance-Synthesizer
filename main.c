eca51490e08b6f3e75c1a81b4ee9b58f39ccc63b
Derek Thrasher <Thrasher8390@gmail.com>
2014-09-25 21:44:59 -0400

    Moved Functions to Headers and Source Files

--- main.c
+++ main.c
@@ -21,24 +21,9 @@
 // This is part of revision 1.1 of the EK-TM4C123GXL Firmware Package.
 //
 //*****************************************************************************
-#include "inc/tm4c123gh6pm.h"
+#include "main.h"
+#include "LEDDistanceSynthesizer.h"
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "inc/hw_memmap.h"
-#include "inc/hw_nvic.h"
-#include "inc/hw_types.h"
-#include "driverlib/debug.h"
-#include "driverlib/fpu.h"
-#include "driverlib/gpio.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/rom.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/systick.h"
-#include "driverlib/uart.h"
-#include "utils/uartstdio.h"
-#include "driverlib/timer.h"
 
 //*****************************************************************************
 //
@@ -59,57 +44,26 @@
 //
 //*****************************************************************************
 
-typedef enum
-{
-	FALSE,
-	TRUE
-}BOOLEAN;
 
-//Typedef for LED.h
-	typedef enum
-	{
-		LED_OFF,
-		LED_RED,
-		LED_RED_GREEN,
-		LED_GREEN,
-		LED_GREEN_BLUE,
-		LED_BLUE,
-		LED_RED_BLUE,
-		LED_RED_GREEN_BLUE,
-		//This number must always be last!!!!!
-		LED_MAX_NUMBER_COLORS
-	}LED_COLOR;
-#define LED_PORT			(GPIO_PORTF_BASE)
-#define LED_RED_PIN 		(GPIO_PIN_1)
-#define LED_GREEN_PIN		(GPIO_PIN_3)
-#define LED_BLUE_PIN		(GPIO_PIN_2)
-
-#define ULTRASONIC_SENSOR_PORT	(GPIO_PORTD_BASE)
-#define ECHO_PIN				(GPIO_PIN_3)
-#define TRIGGER_PIN				(GPIO_PIN_6)
-
-#define CLEAR	(0)
-#define SET		(1)
 //*****************************************************************************
 //		GLOBAL FUNCTION PROTOTYPES
 //*****************************************************************************
-void LED__ChangeColor(LED_COLOR);	//LED.c
+#include "LED.h"
 void IntGPIOd(void);				//Interupts.c
-void Delay(uint32_t);   //HelperFunctions.c
-void DelayMS(uint32_t);   //HelperFunctions.c
-void Ultrasonic__SetTrigger(void);	//Ultrasonic.c
-void Timers__Initialize(void);			//Timers.c
-void GPIO_Initialization(void);		//GPIO.c
+#include "HelperFunctions.h"
+#include "Timers.h"
+#include "GPIO.h"
+#include "Ultrasonic.h"
+
 //*****************************************************************************
 //		LOCAL FUNCTION PROTOTYPES
 //*****************************************************************************
 static void configureUART(void);
-void IntGPIOd(void);
+
 //*****************************************************************************
 //		GLOBAL DATA VARIABLES
 //*****************************************************************************
-BOOLEAN WaitingForEcho = FALSE;
-int DistanceCM = 0;
+
 //*****************************************************************************
 //
 // The error routine that is called if the driver library encounters an error.
@@ -195,7 +149,7 @@ int main(void)
 		Ultrasonic__SetTrigger();
 		//while(WaitingForEcho);
 		//Delay(1);
-		switch((DistanceCM % 14)/2)
+		switch((Ultrasonic_GetDistanceCM() % 14)/2)
 		{
 		case 0:
 			LED__ChangeColor(LED_RED);
@@ -226,62 +180,7 @@ int main(void)
 	}
 }
 
-void LED__ChangeColor(LED_COLOR color)
-{
-	switch(color)
-	{
-		case LED_OFF:
-		{
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_GREEN_PIN | LED_BLUE_PIN, CLEAR);
-			break;
-		}
-		case LED_RED:
-		{
-			GPIOPinWrite(LED_PORT, LED_RED_PIN, LED_RED_PIN);
-			GPIOPinWrite(LED_PORT, LED_GREEN_PIN | LED_BLUE_PIN, CLEAR);
-			break;
-		}
-		case LED_RED_GREEN:
-		{
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_GREEN_PIN, LED_RED_PIN | LED_GREEN_PIN);
-			GPIOPinWrite(LED_PORT, LED_BLUE_PIN, CLEAR);
-			break;
-		}
-		case LED_GREEN:
-		{
-			GPIOPinWrite(LED_PORT, LED_GREEN_PIN, LED_GREEN_PIN);
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_BLUE_PIN, CLEAR);
-			break;
-		}
-		case LED_GREEN_BLUE:
-		{
-			GPIOPinWrite(LED_PORT, LED_GREEN_PIN | LED_BLUE_PIN, LED_GREEN_PIN | LED_BLUE_PIN);
-			GPIOPinWrite(LED_PORT, LED_RED_PIN, CLEAR);
-			break;
-		}
-		case LED_BLUE:
-		{
-			GPIOPinWrite(LED_PORT, LED_BLUE_PIN, LED_BLUE_PIN);
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_GREEN_PIN, CLEAR);
-			break;
-		}
-		case LED_RED_BLUE:
-		{
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_BLUE_PIN, LED_RED_PIN | LED_BLUE_PIN);
-			GPIOPinWrite(LED_PORT, LED_GREEN_PIN, CLEAR);
-			break;
-		}
-		case LED_RED_GREEN_BLUE:
-		{
-			GPIOPinWrite(LED_PORT, LED_RED_PIN | LED_GREEN_PIN | LED_BLUE_PIN, LED_RED_PIN | LED_GREEN_PIN | LED_BLUE_PIN);
-			break;
-		}
-		default:
-		{
-			break;
-		}
-	}
-}
+
 
 /*!
  * \brief lets make an led blink every 2 seconds
@@ -293,28 +192,7 @@ void Blink_LED_2_Seconds_Test()
 
 
 
-/*!
- *  \brief Init timer module
- */
-#define SYSTEM_TIMER_MHZ 	(16)
-#define TIMER0 				(TIMER0_BASE)
-#define TIMER0_PRESCALER 	(16)
-#define TIMER0_COUNT_US()	(TimerValueGet(TIMER0, TIMER_A)/SYSTEM_TIMER_MHZ)
-void Timers__Initialize(void)
-{
-	//DisableTimer
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-	TimerDisable(TIMER0, TIMER_A);
 
-	//Periodic / TAMIE
-	TimerConfigure(TIMER0, TIMER_CFG_A_PERIODIC);
-
-	//Set prescaler. Not really sure what clock we are running from
-	TimerPrescaleSet(TIMER0, TIMER_A, TIMER0_PRESCALER);
-
-	//Enable Timer
-	TimerEnable(TIMER0, TIMER_A);
-}
 /*!
  * \brief setup the timer for capture compare or to fire every 2 seconds
  * \note make sure that the prescale times the 16 MHz clock results in a tick that can be equated to 2 seconds.
@@ -324,144 +202,11 @@ void initializeCCRTimer()
 
 }
 
-/*!
- * \brief Init GPIO
- */
-void GPIO_Initialization(void)
-{
-	//
-	// Enable the peripherals used by this example.
-	//
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
 
 
-	//Set up Port D Pin 3 (PD3) as a both edge interrupt
-	 // Set up our Echo interrupt
-	GPIOPinTypeGPIOInput(ULTRASONIC_SENSOR_PORT, ECHO_PIN);
-	IntMasterEnable();																// Allow interrupts
-	GPIOIntTypeSet(ULTRASONIC_SENSOR_PORT, ECHO_PIN, GPIO_BOTH_EDGES);
-	GPIOIntClear(ULTRASONIC_SENSOR_PORT, ECHO_PIN);
-	GPIOIntEnable(ULTRASONIC_SENSOR_PORT, ECHO_PIN);
-	//
-	// Enable Port D at echo timer.
-	//
-	IntEnable(INT_GPIOD);
-	//
-	// Set the interrupt priorities so they are all equal.
-	//
-	IntPrioritySet(INT_GPIOD, 0x01);
 
 
-	//Set up our Trigger
-	GPIOPinTypeGPIOOutput(ULTRASONIC_SENSOR_PORT, TRIGGER_PIN);
-	GPIOPinWrite(ULTRASONIC_SENSOR_PORT, 0xFF, CLEAR);
 
-	//
-	// Configure the PF1-PF3 to be outputs to indicate entry/exit of one
-	// of the interrupt handlers.
-	//
-	GPIOPinTypeGPIOOutput(LED_PORT, LED_RED_PIN | LED_GREEN_PIN | LED_BLUE_PIN);
-}
-//*****************************************************************************
-//
-// Delay for the specified number of seconds.  Depending upon the current
-// SysTick value, the delay will be between N-1 and N seconds (i.e. N-1 full
-// seconds are guaranteed, aint32_t with the remainder of the current second).
-//
-//*****************************************************************************
-void Delay(uint32_t ui32Seconds)
-{
-	//
-	// Loop while there are more seconds to wait.
-	//
-	while (ui32Seconds--) {
-		//
-		// Wait until the SysTick value is less than 1000.
-		//
-		while (SysTickValueGet() > 1000) {
-		}
-
-		//
-		// Wait until the SysTick value is greater than 1000.
-		//
-		while (SysTickValueGet() < 1000) {
-		}
-	}
-}
-void DelayMS(uint32_t ui32MilliSeconds)
-{
-	//
-	// Loop while there are more seconds to wait.
-	//
-	while (ui32MilliSeconds--)
-	{
-		//
-		// Wait until the SysTick value is less than 1000.
-		//
-		while (SysTickValueGet() > 1)
-		{
-		}
-
-		//
-		// Wait until the SysTick value is greater than 1000.
-		//
-		while (SysTickValueGet() < 1)
-		{
-		}
-	}
-}
-//*****************************************************************************
-//
-// This is the handler for INT_GPIOC.  It triggers INT_GPIOB and saves the
-// interrupt sequence number.
-//
-//*****************************************************************************
-static BOOLEAN risingEdgeSeen = FALSE;
-static uint16_t beginTime, echoWidth;
-void IntGPIOd(void)
-{
-	GPIOIntClear(ULTRASONIC_SENSOR_PORT, ECHO_PIN);
-	//Rising Edge
-	if(risingEdgeSeen == FALSE)
-	{
-		beginTime = TIMER0_COUNT_US();
-		risingEdgeSeen = TRUE;
-	}
-	else
-	{
-		//Counts down
-		echoWidth = beginTime - TIMER0_COUNT_US();
-		UARTprintf("Pulse width = %d\n",echoWidth);
-		//Need to read a timer at rising and falling edge to determine the distance of the ping
-		//cm = microseconds/29/2
-		//in = microseconds/72/2
-		DistanceCM = (echoWidth/29/2);
-		UARTprintf("Centimeters = %d\n", DistanceCM);
-		risingEdgeSeen = FALSE;
-
-		//We have heard the echo
-		WaitingForEcho = FALSE;
-	}
-}
-
-/*!
- * \brief Will set triger pin high for x ms.
- */
-void Ultrasonic__SetTrigger(void)
-{
-	uint32_t i;
-	//Set the trigger pin high
-	GPIOPinWrite(ULTRASONIC_SENSOR_PORT, TRIGGER_PIN, TRIGGER_PIN);
-	//wait some time
-	for(i = 0; i<400; i++)
-	{
-
-	}
-	//Set the trigger pin back to low
-	GPIOPinWrite(ULTRASONIC_SENSOR_PORT, TRIGGER_PIN, CLEAR);
-	WaitingForEcho = TRUE;
-}
 
 //*****************************************************************************
 //
